f540bb8acf336ed703694e3f6dd30bcacaf31cd9
Helen Haveloh <h.havelohh@gmail.com>
2025-07-08 11:55:26 +0200

    added photo example

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("Das Skript wird geladen");
 
+// Button Event Listener für Chuck Norris Witze
 document.getElementById("load").addEventListener('click', async () => {
     const p = document.getElementById('joke');
     const img = document.getElementById('icon');
@@ -17,3 +18,32 @@ document.getElementById("load").addEventListener('click', async () => {
         p.textContent = "Fehler beim Laden"
     }
 })
+
+
+// Button Event Listener für unsere Foto Galerie
+document.getElementById("load-photos").addEventListener('click', async () => {
+    const galleryContainer = document.getElementById("photo-gallery");
+    const button = document.getElementById('load-photos');
+
+    button.disabled = true;
+    button.textContent = "Lädt Fotos.."
+    galleryContainer.innerHTML = "<p> Lade die wunderschönen Fotos... </p>"
+
+    // Jetzt wollen wir die Fotos von der API fetchen
+    try {
+        const response = await fetch("https://picsum.photos/v2/list?page=2&limit=1");
+        const data = await response.json();
+        console.log(data);
+        galleryContainer.innerHTML = `<img height="200" width="200" src="${data[0].download_url}">`;
+    } catch (error) {
+        console.error(error);
+        galleryContainer.innerHTML = "Foto konnte nicht geladen werden";
+    } finally {
+        button.disabled = false;
+        button.textContent = "Foto laden";
+    }
+
+})
+
+
+
