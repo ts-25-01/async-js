f275154007816977c9c89e15489f613f909b0572
Helen Haveloh <h.havelohh@gmail.com>
2025-07-08 12:49:36 +0200

    added photo example with foreach example

--- main.js
+++ main.js
@@ -31,10 +31,32 @@ document.getElementById("load-photos").addEventListener('click', async () => {
 
     // Jetzt wollen wir die Fotos von der API fetchen
     try {
-        const response = await fetch("https://picsum.photos/v2/list?page=2&limit=1");
-        const data = await response.json();
-        console.log(data);
-        galleryContainer.innerHTML = `<img height="200" width="200" src="${data[0].download_url}">`;
+        const response = await fetch("https://picsum.photos/v2/list?page=2&limit=10");
+        const photos = await response.json();
+        console.log(photos);
+        // galleryContainer.innerHTML = `<img height="200" width="200" src="${photos[0].download_url}">`;
+        // Wir leeren die Galerie
+        galleryContainer.innerHTML = '';
+        // Fotos hinzufügen, indem wir über das photos-Array iterieren
+        photos.forEach(photo => {
+            const photoDiv = document.createElement('div');
+            photoDiv.innerHTML = `
+             <img src="${photo.download_url}" width="200" height="200">
+            `
+            galleryContainer.appendChild(photoDiv);
+        });
+
+        // in einer normalen for-Schleife
+        // for (let i = 0; i < photos.length; i++){
+        //     const photo = photos[i];
+        //     const photoDiv = document.createElement('div');
+        //     photoDiv.innerHTML = `
+        //      <img src="${photo.download_url}" width="200" height="200">
+        //     `
+        //     galleryContainer.appendChild(photoDiv);
+
+        // }
+
     } catch (error) {
         console.error(error);
         galleryContainer.innerHTML = "Foto konnte nicht geladen werden";
